96bd110832ebf51aecc0449fcb2b0df3e899ec19
Rituparno Das <rituparnodas.datacoresystems@gmail.com>
2024-02-27 17:00:01 +0530

    First Commit

---
+++ E-commerce/middleware/token.js
@@ -0,0 +1,36 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+app.use(cookieParser());
+
+
+
+
+// Cookie verification middleware
+const verifyToken = (req, res, next) => {
+
+    // console.log("HERE");
+    // Extract token from the Authorization header
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        // No token found, send 401 Unauthorized response
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            // Invalid token, send 401 Unauthorized response
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        // Token is valid, proceed to the next middleware or route handler
+        req.userId = decoded.userId;
+        req.email = decoded.email;
+        next();
+    });
+};
---
+++ E-commerce/src/models/index.js
@@ -0,0 +1,25 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize('flipzon', 'root', '', {
+  host: 'localhost',
+//   port: 3307,
+  logging: true,
+  dialect: 'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+});
+
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.user = require("./user")(DataTypes, sequelize, Model)
+
+db.sequelize.sync({ force: true });
+
+module.exports = db
