f7ee11e6fe07610c4d18a9d754ccb022a6e9ce97
Rituparno Das <rituparnodas.datacoresystems@gmail.com>
2024-04-12 16:42:22 +0530

    commit

--- School/src/controllers/Gradecontroller.js
+++ School/src/controllers/Gradecontroller.js
@@ -1,12 +1,36 @@
 const { Op } = require("sequelize");
 const gradeStructure = require('../models/gradeStructure');
+const grade = require('../models/grade');
 const Class = require('../models/class');
 const ExamCategory = require('../models/examCategory');
+const academicYear = require('../models/academicYear');
 const { sendSuccessResponse, sendRecordsResponse, sendErrorResponse } = require('../utils/response')
 const { validationErrorCode, unauthErrorCOde, notfoundErrorCode, successCode, serverErrorCode } = require('../utils/statuscode');
 
+// grade.belongsto(gradeStructure,{foreignkey: ""})
+Class.belongsto(gradeStructure,{foreignkey: "t_rel_class_id"})
+ExamCategory.belongsto(gradeStructure,{foreignkey: "t_rel_exam_category_id"})
+academicYear.belongsto(gradeStructure,{foreignkey: "t_mst_academic_year_id"})
 
-
+var AllGrades = async (req, res) => {
+    try {
+        var data = await Section.findAll({
+            
+        });
+        sendRecordsResponse(
+            res,
+            successCode,
+            "data get successfully",
+            data
+        );
+    } catch (error) {
+        return sendErrorResponse(
+            res,
+            serverErrorCode,
+            "Internal server error!",
+        );
+    }
+}
 
 module.exports = {
 
